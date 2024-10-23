42abd82f80505e5f7f248a544f08f043b4da2f64
Zhanetta11 <tynymbekova1112@gmail.com>
2024-10-23 23:07:17 +0600

    Restored all files from other branches to main branch

---
+++ Shopping list/app.js
@@ -0,0 +1,34 @@
+const addItemBtn = document.getElementById('add-item-btn');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+const items = [];
+
+addItemBtn.addEventListener('click', function() {
+    const itemName = itemInput.value.trim(); // Получаем текст из input
+
+    if (itemName !== "") {
+        const newItem = {
+            name: itemName,
+            purchased: false
+        };
+
+        items.push(newItem);
+
+        const li = document.createElement('li');
+        li.textContent = itemName;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+
+        deleteBtn.addEventListener('click', function() {
+            itemList.removeChild(li.nextSibling)
+        });
+
+        li.appendChild(deleteBtn);
+
+        itemInput.value = "";
+    } else {
+        alert("Please enter an item name.");
+    }
+});
