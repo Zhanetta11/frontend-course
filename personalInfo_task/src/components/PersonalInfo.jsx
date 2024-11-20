e6ee0ed37527b3d4dbe393de6276fb60f59758a9
Zhanetta11 <tynymbekova1112@gmail.com>
2024-11-20 18:55:02 +0600

    feat: add components with personal information

---
+++ personalInfo_task/src/components/PersonalInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PersonalInfo.css';
+
+const PersonalInfo = () => {
+    return (
+        <div className="personal-info">
+            <h1 className="name">Alim Alimbekovich</h1>
+            <p className="position">Frontend Developer</p>
+            <p> <strong>Company:</strong> MegaCom</p>
+            <p> <strong>Age:</strong> 28 years old</p>
+            <p> <strong>Experience:</strong> 8 years</p>
+            <p> <strong>Place of residence:</strong> Bishkek, Kyrgyzstan</p>
+        </div>
+    );
+};
+export default PersonalInfo;
