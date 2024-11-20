a3adb0d5dda89ba5aa4a1469efa1a416e15d0482
Zhanetta11 <tynymbekova1112@gmail.com>
2024-11-20 19:48:00 +0600

    feat: add functionality to the button, it changes the position

--- personalInfo_task/src/components/PersonalInfo.jsx
+++ personalInfo_task/src/components/PersonalInfo.jsx
@@ -3,21 +3,23 @@ import './PersonalInfo.css';
 
 const PersonalInfo = () => {
     const [messageVisible, setMessageVisible] = useState(false);
+    const [position, setPosition] = useState('Middle Frontend Developer');
 
     const handleClick = () => {
         setMessageVisible(true);
+        setPosition('Senior Frontend Developer');
     };
 
     return (
         <div className="personal-info">
             <h1 className="name">Alim Alimbekovich</h1>
-            <p className="position">Frontend Developer</p>
+            <p className="position">{position}</p>
             <p> <strong>Company:</strong> MegaCom</p>
             <p> <strong>Age:</strong> 28 years old</p>
             <p> <strong>Experience:</strong> 8 years</p>
             <p> <strong>Place of residence:</strong> Bishkek, Kyrgyzstan</p>
 
-            {messageVisible && <p className="message">You clicked the button</p>}
+            {messageVisible && <p className="message">You have successfully promoted the position</p>}
 
             <button className="btn" onClick={handleClick}>
                 Promote
