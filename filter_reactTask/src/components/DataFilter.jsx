a567b80d73128201f298b6e256cce720f68734f5
Zhanetta11 <tynymbekova1112@gmail.com>
2024-11-27 18:21:40 +0600

    Initialize data list on first render using useEffect

--- filter_reactTask/src/components/DataFilter.jsx
+++ filter_reactTask/src/components/DataFilter.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './DataFilter.css';
 
-const DataFilter = () => {
-  const [data, setData] = useState([]); 
-  const [searchQuery, setSearchQuery] = useState(''); 
+const nameData = [
+    { id: 1, name: 'Azamat' },
+    { id: 2, name: 'Bektur' },
+    { id: 3, name: 'Ali' },
+    { id: 4, name: 'Dastan' },
+    { id: 5, name: 'Nuremir' },
+    { id: 6, name: 'Ruslan' },
+    { id: 7, name: 'Ulan' },
+    { id: 8, name: 'Sanjar' },
+    { id: 9, name: 'Ilnar' },
+    { id: 10, name: 'Mirbek' },
+];
 
-  return <div>Data filtering</div>;
+const DataFilter = () => {
+    const [data, setData] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    useEffect(() => {
+        setData(nameData);
+    }, []);
+    return <div>Data filtering</div>;
 };
 
 export default DataFilter;
