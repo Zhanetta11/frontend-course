6f07554febd4e6fb8caee7a863f1f485584428c0
Zhanetta11 <tynymbekova1112@gmail.com>
2024-11-27 16:22:20 +0600

    feat: create state for data and search query

---
+++ filter_reactTask/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataFilter from './components/DataFilter';
+
+const App = () => {
+  return (
+    <div>
+      <DataFilter />
+    </div>
+  );
+};
+
+export default App;
---
+++ filter_reactTask/src/components/DataFilter.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './DataFilter.css';
+
+const DataFilter = () => {
+  const [data, setData] = useState([]); 
+  const [searchQuery, setSearchQuery] = useState(''); 
+
+  return <div>Data filtering</div>;
+};
+
+export default DataFilter;
