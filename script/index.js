8f1d48a7d27cc641fd9c58296c82569d67c216de
Zhanetta11 <tynymbekova1112@gmail.com>
2024-12-12 20:44:41 +0600

    feat: Add search for a city. Show basic data

---
+++ script/index.js
@@ -0,0 +1,35 @@
+const API = 'https://api.openweathermap.org/data/2.5/weather?q='
+const apiKey = '&appid=2fe336be3244a86cb9176e98a52f60a6'
+
+const form = document.querySelector('.search')
+const input = document.querySelector('.inp')
+const output = document.querySelector('.output')
+
+const getWeatherData = async () => {
+    const url = API + input.value + apiKey
+    const request = await fetch(url)
+    const response = await request.json()
+    renderData(response);
+    input.value = ''
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    getWeatherData()
+
+})
+
+const renderData = (data) => {
+    console.log(data);
+    output.innerHTML = ''
+    const cityName = document.createElement('h1')
+    cityName.textContent = data.name
+
+    const tempC = document.createElement('h3')
+    tempC.textContent = `Temperature: ${Math.floor(data.main.temp - 273.15)}`
+
+    const weatherCondition = document.createElement('h4')
+    weatherCondition.textContent = `Weather Condition: ${data.weather[0].main}`;
+
+    output.append(cityName, tempC, weatherCondition)
+}
